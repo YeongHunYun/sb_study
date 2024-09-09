a8a1f0cb82ee8d48201206286c2ce91fc2946150
YeongHunYun <compyyh@gmail.com>
2024-09-09 15:31:40 +0900

    2.3

---
+++ src/main/java/org/suhodo/sb_study/SbStudyApplication.java
@@ -0,0 +1,16 @@
+package org.suhodo.sb_study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbStudyApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(SbStudyApplication.class, args);
+
+
+    }
+
+}
---
+++ src/test/java/org/suhodo/sb_study/SbStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package org.suhodo.sb_study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
